703af50158e97e54dea52eadb9ee30feffb80838
Stephen Rooney <s.f.rooney@gmail.com>
2018-08-03 23:57:30 +0100

    mouseover functionality added

--- js/app.js
+++ js/app.js
@@ -5,36 +5,62 @@ let lastName = '';
 let address = '';
 let pizza = 'MAN I WANT A PIZZA';
 let temperature = '';
+ let description = document.querySelector('#description');
 //this is the refactor and I am seperating each event
-const firstNameInput = document.querySelector('#first_name')
+
+const firstNameInput = document.querySelector('#first_name');
 firstNameInput.addEventListener('input', (event) =>{
   console.log(event.target.value);
 
   firstName = event.target.value;
 
-})
+});
 
-const lastNameInput = document.querySelector('#last_name')
+const lastNameInput = document.querySelector('#last_name');
 lastNameInput.addEventListener('input', (event) =>{
     lastName = event.target.value;
-})
+});
 
-const addressInput = document.querySelector('#address')
+const addressInput = document.querySelector('#address');
 addressInput.addEventListener('input', (event) => {
   address = event.target.value;
-})
+});
 
 
-const pizzaInput = document.querySelector('#pizza_type')
+const pizzaInput = document.querySelector('#pizza_type');
 pizzaInput.addEventListener('change', (event) =>{
   pizza = event.target.value;
-})
+});
 
 const tempInput = document.querySelector('fieldset')
  tempInput.addEventListener('input', (event) => {
 
    temperature = event.target.value;
- })
+ });
+
+ const margheritaHover = document.querySelector('#margherita');
+ margheritaHover.addEventListener('mouseover', ()=> {
+
+   description.textContent = "The Margherita: the first one we could think of! It's an oldy but a goody! Sometimes";
+});
+
+ const onionHover = document.querySelector('#onions');
+ onionHover.addEventListener('mouseover', ()=> {
+
+   description.textContent = "Onions and toothpaste: all of the zing of onions with cool refreshing toothpaste";
+ });
+
+ const cornflakeHover = document.querySelector('#weetabix');
+ cornflakeHover.addEventListener('mouseover', ()=> {
+
+   description.textContent = "Cornflakes surprise! It's a crunchy treat! And the surprise? - it's actually Weetabix"
+ });
+
+ const potatoHover = document.querySelector('#potato');
+ potatoHover.addEventListener('mouseover', ()=> {
+
+   description.textContent = "Ah! Potatoes and Pepperoni: strongly recommended to be eaten hot and not cold";
+ });
 
 
 
